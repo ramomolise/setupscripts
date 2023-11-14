43715643662859f10520a0bc935fc3fc81f6e4ea
Ramo Molise <129608647+ramomolise@users.noreply.github.com>
2023-11-14 12:53:45 +0200

    new script

---
+++ removeLocalLVMProxmox.sh
@@ -0,0 +1,7 @@
+read -p "Make sure you've deleted 'local-lvm' on proxmox web interface. Press any key to continue o Ctrl-C to quit"
+
+lvremove /dev/pve/data
+lvresize -l +100%FREE /dev/pve/root
+resize2fs /dev/mapper/pve-root
+
+echo "'local' has been resized"
