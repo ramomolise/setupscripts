2bdd3bc4ccbc6986534e05e9f68294db7c4ccd36
Ramo Molise <129608647+ramomolise@users.noreply.github.com>
2023-11-23 09:27:49 +0200

    New AutoStart file for quick setup

---
+++ autoStart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode VGA-1 "1920x1080_60.00"
+xrandr -s "1920x1080_60.00"
+
+sleep 10
+
+firefox --kiosk https://flixwave.to/home
